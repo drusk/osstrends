a22977de0e565c065aad91ff35afcfefecfbe895
drusk <drusk@uvic.ca>
2013-10-03 11:05:06 -0700

    Changed HTML title to OSS Trends, updated header and tweaked formatting.  Also convert location to lower-case before searching in database.

--- osstrends/web/__init__.py
+++ osstrends/web/__init__.py
@@ -38,7 +38,7 @@ def location_selection():
 @app.route("/users")
 def users_by_location():
     location = request.args["location"]
-    users = db.get_users_by_location(location)
+    users = db.get_users_by_location(location.lower())
     return render_template("users.html", location=location, users=users)
 
 
